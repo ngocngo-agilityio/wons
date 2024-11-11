33a5114440b015d4394ca714e58112c10bd1a135
Linh Thi Le <linh.lethi@asnet.com.vn>
2024-11-11 15:17:38 +0700

    Update Search input

--- src/ui/schedule-list/ScheduleListActions/index.tsx
+++ src/ui/schedule-list/ScheduleListActions/index.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 // components
 import { SearchInput, TaskDrawer } from '@/components';
 
-const ScheduleListActions = async (): Promise<JSX.Element> => (
+const ScheduleListActions = () => (
   <div className="flex items-center base:gap-10 md:gap-5 mt-10 md:mt-0 base:flex-col md:flex-row">
     <SearchInput className="base:w-full md:w-fit" />
 
