83dbac0aff9e30b7570d329c1e545a08645f7800
Huy Anh Dao <huy.dao@asnet.com.vn>
2024-11-13 10:53:46 +0700

    hotfix: add metadata

--- src/app/(auth)/account-success/page.tsx
+++ src/app/(auth)/account-success/page.tsx
@@ -1,4 +1,25 @@
+import { Metadata } from 'next';
+
+// Constants
+import { IMAGES } from '@/constants';
+
+// UI
 import { AccountSuccess } from '@/ui';
+
+export const metadata: Metadata = {
+  title: 'Wons Account Success',
+  description: 'Confirm account registration is successful',
+  openGraph: {
+    title: 'Wons Account Success',
+    description: 'Confirm account registration is successful',
+    images: [
+      {
+        url: IMAGES.PREVIEW_IMAGE,
+        alt: 'preview image',
+      },
+    ],
+  },
+};
 
 const AccountSuccessPage = () => <AccountSuccess />;
 
