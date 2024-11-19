c6d626334f6881e12c0eb3b0b1943ff1a08fc3fa
tantran-agilityio <tan.tran@asnet.com.vn>
2024-11-19 17:55:54 +0700

    style: update close drawer button for CustomerDetails and ProductDetails

--- src/components/CustomerDetails/index.tsx
+++ src/components/CustomerDetails/index.tsx
@@ -1,20 +1,27 @@
 import { memo } from 'react';
 import isEqual from 'react-fast-compare';
-
 // Models
 import { ICustomer } from '@/models';
+
+// Icons
+import { GrPrevious } from 'react-icons/gr';
 
 // Components
 import CustomerDetailsHeader from './header';
 import CustomerDetailsBody from './body';
-import { LoadingIndicator } from '../common';
+import { LoadingIndicator, Button } from '../common';
 
 interface ICustomerDetails {
   customer: ICustomer;
   isLoading?: boolean;
+  onCloseDrawer?: () => void;
 }
 
-const CustomerDetails = ({ customer, isLoading = false }: ICustomerDetails) => {
+const CustomerDetails = ({
+  customer,
+  isLoading = false,
+  onCloseDrawer,
+}: ICustomerDetails) => {
   const {
     avatar = '',
     firstName = '',
@@ -25,11 +32,18 @@ const CustomerDetails = ({ customer, isLoading = false }: ICustomerDetails) => {
     phone = '',
     address = '',
   } = customer ?? {};
-
   return isLoading ? (
     <LoadingIndicator />
   ) : (
-    <div className="min-h-full max-w-[302px] bg-white dark:bg-gray-400 py-[62px] px-6">
+    <div className="min-h-full w-full bg-white dark:bg-gray-400 py-[62px] px-6">
+      <Button
+        data-testid="close-product-form"
+        onClick={onCloseDrawer}
+        className="p-2 bg-transparent dark:bg-transparent text-gray-200 dark:text-gray-300 hover:bg-transparent dark:hover:bg-transparent"
+      >
+        <GrPrevious size={20} />
+      </Button>
+
       <CustomerDetailsHeader
         avatar={avatar}
         firstName={firstName}
@@ -37,6 +51,7 @@ const CustomerDetails = ({ customer, isLoading = false }: ICustomerDetails) => {
         fullName={fullName}
         job={job}
       />
+
       <CustomerDetailsBody email={email} phone={phone} address={address} />
     </div>
   );
