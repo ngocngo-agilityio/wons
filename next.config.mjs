bf86d65ef7942dc08e2c927ba33ea0c34dd88030
Linh Thi Le <linh.lethi@asnet.com.vn>
2024-11-21 17:18:26 +0700

    Update Security next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.gstatic.com;
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self' https://fonts.gstatic.com;
